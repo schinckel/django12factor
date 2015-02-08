631f9b91ade3ab2b648c4dfa2c82ab793c6ed43e
Kristian Glass <git@doismellburning.co.uk>
2015-02-08 01:29:25 +0000

    Set `stdout` logger's stream to actually use `stdout`!
    https://github.com/doismellburning/django12factor/issues/12
    
    https://docs.python.org/2/library/logging.handlers.html#logging.StreamHandler
    
    "If stream is specified, the instance will use it for logging output;
    otherwise, sys.stderr will be used." - oops

--- django12factor/__init__.py
+++ django12factor/__init__.py
@@ -43,6 +43,7 @@ def factorise(custom_settings=None):
             'stdout': {
                 'level': 'DEBUG',
                 'class': 'logging.StreamHandler',
+                'stream': 'ext://sys.stdout',
             }
         },
         'root': {
